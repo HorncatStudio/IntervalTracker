487171d62835303d08ad6519fce1a2aea27da231
Shinichi <uno.shinichi11@gmail.com>
2014-06-16 22:37:40 -0500

    Changed buttons to toggle buttons, and refactored.

--- app/src/main/java/com/example/hcrunning/app/HcRunningActivity.java
+++ app/src/main/java/com/example/hcrunning/app/HcRunningActivity.java
@@ -1,99 +1,76 @@
 package com.example.hcrunning.app;
 
 import android.app.Activity;
-import android.media.MediaPlayer;
 import android.os.Bundle;
-import android.os.CountDownTimer;
 import android.view.View.OnClickListener;
 import android.view.View;
 import android.widget.Button;
 import android.widget.NumberPicker;
 import android.widget.TextView;
+import android.widget.ToggleButton;
 
 public class HcRunningActivity extends Activity {
-
-    public TextView mCurrentTimeTextView;
-
-    //private MyCountDownTimer mCountDownTimer = new MyCountDownTimer(3 * 1000, 50);
+    private TextView mCurrentTimeTextView;
     private MyCountDownTimer mCountDownTimer;
-
     private NumberPicker mNumberPicker;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_hc_running);
+      super.onCreate(savedInstanceState);
+      setContentView(R.layout.activity_hc_running);
 
-        mCurrentTimeTextView = (TextView) findViewById(R.id.textView);
+      this.mCurrentTimeTextView = (TextView) findViewById(R.id.textView);
 
-        mNumberPicker = (NumberPicker) findViewById(R.id.numberPicker);
+      this.mCountDownTimer = new MyCountDownTimer(mNumberPicker.getValue() * 1000, 50, mCurrentTimeTextView, this);
 
-        mNumberPicker.setMinValue(1);
-        mNumberPicker.setMaxValue(20);
-        mNumberPicker.setWrapSelectorWheel(false);
-        mNumberPicker.setValue(10);
+      this.mNumberPicker = (NumberPicker) findViewById(R.id.numberPicker);
+      this.mNumberPicker.setMinValue(1);
+      this.mNumberPicker.setMaxValue(20);
+      this.mNumberPicker.setWrapSelectorWheel(false);
+      this.mNumberPicker.setValue(10);
 
-       // mCountDownTimer = new MyCountDownTimer(Integer.parseInt(mNumberPicker.getDisplayedValues()) * 1000, 50);
+//      Button startActivationButton = (Button) findViewById(R.id.start_activation_button);
+//      startActivationButton.setOnClickListener(new OnClickListener() {
+//        @Override
+//        public void onClick(View v) {
+//          mCountDownTimer.start();
+//        }
+//      });
 
-        Button startActivationButton = (Button) findViewById(R.id.start_activation_button);
-        startActivationButton.setOnClickListener(new OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                mCountDownTimer = new MyCountDownTimer( mNumberPicker.getValue() * 1000, 50);
-                mCountDownTimer.start();
-            }
-        });
-
-        Button stopActivationButton = (Button) findViewById(R.id.stop_activation_button);
-        stopActivationButton.setOnClickListener(new OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                mCountDownTimer.cancel();
-            }
-        });
-
-        Button restartActivationButton = (Button) findViewById(R.id.restart_activation_button);
-        restartActivationButton.setOnClickListener(new OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String currentTime = mCurrentTimeTextView.getText().toString();
-                String[] currentSplitTime = currentTime.split(":", 0);
-                int currentMinute = Integer.parseInt(currentSplitTime[0]);
-                int currentSeconds = Integer.parseInt(currentSplitTime[1]);
-                int currentTotalInSeconds = (currentMinute * 60) + currentSeconds;
-                mCountDownTimer = new MyCountDownTimer( currentTotalInSeconds * 1000, 1000);
-                mCountDownTimer.start();
-            }
-        });
-
+//        Button restartActivationButton = (Button) findViewById(R.id.pauseContinue_ToggleButton);
+//        restartActivationButton.setOnClickListener(new OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                mCountDownTimer = new MyCountDownTimer(mCountDownTimer.getCurrentTimeInSeconds() * 1000, 1000, mCurrentTimeTextView);
+//                mCountDownTimer.start();
+//            }
+//        });
     }
 
-    public class MyCountDownTimer extends CountDownTimer {
-        public MyCountDownTimer(long millisInFuture, long countdownInterval) {
-            super(millisInFuture, countdownInterval);
-        }
-
-        @Override
-        public void onFinish() {
-            mCurrentTimeTextView.setText("Done!");
-            final MediaPlayer mPlayer = MediaPlayer.create(getApplicationContext(), R.raw.coinsound);
-            mPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
-                @Override
-                public void onCompletion(MediaPlayer mp) {
-                      mp.release();
-                }
-            });
-            try {
-                mPlayer.start();
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
-
-        @Override
-        public void onTick(long millisUntilFinished) {
-            mCurrentTimeTextView.setText(Long.toString(millisUntilFinished / 1000 / 60) + ":" + Long.toString((millisUntilFinished / 1000 % 60) + 1));
-        }
+    public void startAndCancelToggleButton(View view) {
+      ToggleButton pauseContinueButtonActivator = (ToggleButton) findViewById(R.id.pauseContinue_ToggleButton);
+      boolean on = ((ToggleButton) view).isChecked();
+      if (on) {
+        //Cancel state
+        pauseContinueButtonActivator.setClickable(false);
+        this.mCountDownTimer.clearCurrrentTime(this.mCurrentTimeTextView);
+      } else {
+        //Start state
+        pauseContinueButtonActivator.setClickable(true);
+        this.mCountDownTimer.start();
+      }
     }
+
+    public void pauseAndContinueToggleButton(View view) {
+      boolean on = ((ToggleButton) view).isChecked();
+        if (on) {
+          //Continue state
+          this.mCountDownTimer = new MyCountDownTimer(this.mCountDownTimer.getCurrentTimeInSeconds() * 1000, 50, mCurrentTimeTextView, this);
+          this.mCountDownTimer.start();
+        } else {
+          //Pause state
+          this.mCountDownTimer.cancel();
+        }
+      }
 
 }
---
+++ app/src/main/java/com/example/hcrunning/app/MyCountDownTimer.java
@@ -0,0 +1,57 @@
+package com.example.hcrunning.app;
+
+import android.app.Activity;
+import android.media.MediaPlayer;
+import android.os.CountDownTimer;
+import android.widget.TextView;
+
+/**
+ * Created by Shinichi on 2014/06/13.
+ */
+public class MyCountDownTimer extends CountDownTimer {
+
+  private TextView mTimeTextView = null;
+  private Activity mContext = null;
+
+  public MyCountDownTimer(long millisInFuture, long countdownInterval, TextView timeTextView, Activity context) {
+    super(millisInFuture, countdownInterval);
+    mTimeTextView = timeTextView;
+    mContext = context;
+  }
+
+  public long getCurrentTimeInSeconds() {
+    String currentTime = this.mTimeTextView.getText().toString();
+    String[] currentSplitTime = currentTime.split(":", 0);
+    long currentMinute = Long.parseLong(currentSplitTime[0]);
+    long currentSeconds= Long.parseLong(currentSplitTime[1]);
+    return (currentMinute * 60) + currentSeconds;
+  }
+
+  public void clearCurrrentTime(TextView currentTime) {
+    currentTime.append("Set time and press start!");
+  }
+
+  @Override
+  public void onFinish() {
+    this.mTimeTextView.setText("Done!");
+    final MediaPlayer mPlayer = MediaPlayer.create(mContext, R.raw.coinsound);
+    mPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+      @Override
+      public void onCompletion(MediaPlayer mp) {
+        mp.release();
+      }
+    });
+    try {
+      mPlayer.start();
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
+
+  @Override
+  public void onTick(long millisUntilFinished) {
+    long minutesPart = millisUntilFinished / 1000 / 60;
+    long secondsPart = (millisUntilFinished / 1000 % 60) + 1;
+    this.mTimeTextView.setText(Long.toString(minutesPart) + ":" + Long.toString(secondsPart));
+  }
+}
