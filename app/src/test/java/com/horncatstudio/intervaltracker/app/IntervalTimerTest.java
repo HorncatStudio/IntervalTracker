2de3f040c6045aa145114a129ba608d3a8dc6230
Sazanaizu33 <uno.shinichi11@gmail.com>
2015-08-12 14:30:47 -0500

    Initial attempt adding unit test

---
+++ app/src/test/java/com/horncatstudio/intervaltracker/app/IntervalTimerTest.java
@@ -0,0 +1,25 @@
+package com.horncatstudio.intervaltracker.app;
+
+import junit.framework.TestCase;
+
+/**
+ * Created by Shinichi on 2015/08/08.
+ */
+public class IntervalTimerTest extends TestCase {
+
+  public void testGetCurrentTimeInSeconds() throws Exception {
+
+  }
+
+  public void testClearCurrentTime() throws Exception {
+
+  }
+
+  public void testOnFinish() throws Exception {
+
+  }
+
+  public void testOnTick() throws Exception {
+
+  }
+}
